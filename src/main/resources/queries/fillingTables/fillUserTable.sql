a0f9152a91c196465ab44e6242470de1b16ee96f
HrisKirill <hristoforow.kirill@gmail.com>
2023-02-08 18:50:25 +0200

    add more users

--- src/main/resources/queries/fillingTables/fillUserTable.sql
+++ src/main/resources/queries/fillingTables/fillUserTable.sql
@@ -1,5 +1,14 @@
 INSERT INTO user (last_name, first_name, login, password, email, role,status)
 VALUES ("Khryst", "Kirill", "kirill", "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5", "hristoforovkirill8@gmail.com", "student","unblocked"),
-       ("Kirienko", "Vlad", "vlad", "e7042ac7d09c7bc41c8cfa5749e41858f6980643bc0db1a83cc793d3e24d3f77", "vlad@gmail.com", "teacher","unblocked"),
        ("Sergies", "Sergey", "sergey", "f6ff5adab5180a3d10ffb611f2caddd0a2b0922bde398ad186e946480bec3943", "sergey@gmail.com", "student","unblocked"),
+       ("Angetle", "Georgie", "Valenci", "3a015ec69ae0b41ca3f8513273b2702d8a97a2939005c83698546c5fcb993192", "student123@gmail.com", "student","unblocked"),
+       ("Kvadric", "Ley", "lervants", "808ec7c652229d754341ac8a62d8bcd8fe9e21dc78658f88bf4d03ea11153486", "student124@gmail.com", "student","unblocked"),
+       ("Lervsat", "Sapler", "servant", "5737c20d5060e21c7a367b38773c6090c21d1a39c95b1061c8d938c18ad39b9e", "student125@gmail.com", "student","unblocked"),
+       ("Tervabt", "Pantervast", "saardina", "781cd6ff2fc900ac3dd15270d8f1739d2dbbbd89985f50febe84f42736655f44", "student126@gmail.com", "student","unblocked"),
+       ("Lormant", "Gvardie", "portevar", "68142cf9e1602953f9b280302516bd92863458bee8001ec4e74b77fc2b3e5616", "student127@gmail.com", "student","unblocked"),
+       ("Berley", "Sebastian", "serbastes", "c5fa36ad4aead9aae8e3cf3c43f8b4fa7f58bcae7779ea4bbb46eec80318b19f", "student128@gmail.com", "student","unblocked"),
+       ("Portaley", "Evgen", "Legardentor", "1468c2377b97b53681cfe27d4b2ddfa3e95d355ae4f6a2f3a6991d4e17a03c08", "student129@gmail.com", "student","unblocked"),
+       ("Kirienko", "Vlad", "vlad", "7eb0f49fb29a124a145b7fde417543cc57856865ce1e4d6bafe257315d27bcda", "teacher101@gmail.com", "teacher","unblocked"),
+       ("Sergienko", "Artem", "Lemur", "c4762b8e1f6bc95d9cc7a280ead5fc4974b56c2209f542aa08ca3a59ccf96020", "teacher102@gmail.com", "teacher","unblocked"),
+       ("Geran", "Karlew", "kardarena", "1a34af4226563ed376b0d3955c2a7f5f2e06bb79eb3ac11b7537ae352bdf8b83", "teacher103@gmail.com", "teacher","unblocked"),
        ("Legan", "Vladimir", "admin", "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918", "vladimir@gmail.com", "sysadmin","unblocked");
