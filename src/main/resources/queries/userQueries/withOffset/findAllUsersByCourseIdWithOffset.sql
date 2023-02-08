52d26e07bd16c548d20bdaa262f2295501d0e25d
HrisKirill <hristoforow.kirill@gmail.com>
2023-02-08 19:23:56 +0200

    fix set grade if student is blocked

--- src/main/resources/queries/userQueries/withOffset/findAllUsersByCourseIdWithOffset.sql
+++ src/main/resources/queries/userQueries/withOffset/findAllUsersByCourseIdWithOffset.sql
@@ -1,5 +1,5 @@
 SELECT * FROM user
 left join users_courses
 on user.id = users_courses.user_id
-where users_courses.course_id = ?
+where users_courses.course_id = ? and user.status = 'unblocked'
 limit ?,?
